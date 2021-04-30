db0725bd8dc3a686941653d56c89070276cea55e
TimWD <timgerm280@gmail.com>
2021-04-30 15:52:23 +0500

    C

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import ManService, WomanService, KidService
 
-# Register your models here.
+
+admin.site.register(ManService)
+admin.site.register(WomanService)
+admin.site.register(KidService)
+
+
+
--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class ManService(models.Model):
+    image = models.ImageField(null=True, blank=True)
+    service_type = models.CharField(blank=True, null=True, max_length=255)
+    price = models.DecimalField(blank=True, null=True, max_digits=7, decimal_places=2)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.service_type
+
+
+class WomanService(models.Model):
+    image = models.ImageField(null=True, blank=True)
+    service_type = models.CharField(blank=True, null=True, max_length=255)
+    price = models.DecimalField(blank=True, null=True, max_digits=7, decimal_places=2)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.service_type
+
+
+class KidService(models.Model):
+    image = models.ImageField(null=True, blank=True)
+    service_type = models.CharField(blank=True, null=True, max_length=255)
+    price = models.DecimalField(blank=True, null=True, max_digits=7, decimal_places=2)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.service_type
+
+
+
