502a0cd7e67b3057cce9d9bd44b0f50fe6d79667
TimWD <timgerm280@gmail.com>
2021-04-30 22:24:55 +0500

    created women, men, kids pages

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.IndexPage.as_view(), name='index'),
+    path('women/', views.WomenPage.as_view(), name='women'),
+    path('men/', views.MenPage.as_view(), name='men'),
+    path('kids/', views.KidsPage.as_view(), name='kids'),
 ]
 
 
--- main_app/views.py
+++ main_app/views.py
@@ -6,4 +6,16 @@ class IndexPage(TemplateView):
     template_name = 'main_app/index.html'
 
 
+class WomenPage(TemplateView):
+    template_name = 'main_app/women.html'
+
+
+class MenPage(TemplateView):
+    template_name = 'main_app/men.html'
+
+
+class KidsPage(TemplateView):
+    template_name = 'main_app/kids.html'
+
+
 
